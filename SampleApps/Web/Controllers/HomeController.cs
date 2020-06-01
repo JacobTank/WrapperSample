f436a86d082c205efdd459c048e933599eb5cf40
Chris Kehayias <chris@thinkministry.com>
2020-06-01 10:27:38 -0400

    Initial Commit

---
+++ SampleApps/Web/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Platform.Clients.PowerService;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {        
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private IPowerService _api;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            
+            // Initialize the PowerServiceClientFactory using configuration data saved in application
+            _api = PowerServiceClientFactory.CreateAsync(new Uri(_configuration["mp.ApiBaseUrl"]), _configuration["mp.client"], _configuration["mp.secret"]).Result;            
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetRecords()
+        {
+            var count = await _api.GetRecordsAsync(
+                table: "Addresses",
+                filter: "Postal_Code IS NOT NULL",
+                select: "COUNT(Addresses.Postal_Code) AS RecordCount, Addresses.Postal_Code AS Zip",
+                groupBy: "Postal_Code",
+                having: "COUNT(Addresses.Postal_Code) > 1000"
+                );
+
+            var contactData = await _api.GetRecordAsync(
+                table: "Contacts",
+                id: 1,
+                select: "Contacts.*, Household_ID_Table.*, Household_ID_Table_Address_ID_Table.*");
+
+            // Get Top 5 Addresses and return as model
+            return View(
+                await _api.GetRecordsAsync<AddressModel>(
+                table: "Addresses",
+                top: 5,
+                skip: 10,
+                filter: "Postal_Code IS NOT NULL AND [State/Region] IS NOT NULL",
+                select: "Addresses.*",
+                orderBy: "Postal_Code"
+                )
+            );
+        }
+
+        public async Task<IActionResult> CreateRecords()
+        {
+            AddressModel model = new AddressModel()
+            {
+                Address_Line_1 = "Test Address Line 1",
+                City = "Test City",
+                State_Region = "AK"
+            };
+
+            List<AddressModel> records = new List<AddressModel>();
+            records.Add(model);
+
+            return View(await _api.CreateRecordsAsync<AddressModel>("Addresses", records));
+        }
+
+        public async Task<IActionResult> UpdateRecords()
+        {
+            var model = await _api.GetRecordAsync<AddressModel>("Addresses", 935752);
+
+            model.Address_Line_1 = "Updated Address Line 1";
+
+            List<AddressModel> recordUpdates = new List<AddressModel>();
+            recordUpdates.Add(model);
+
+            var addressUpdates = await _api.UpdateRecordsAsync<AddressModel>("Addresses", recordUpdates);
+
+            return View(addressUpdates);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleApps/Web/Models/AddressModel.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+	public class AddressModel
+	{
+		[Required]
+		[Key]
+		public int Address_ID { get; set; }
+		[Required]
+		[MaxLength(75)]
+		public string Address_Line_1 { get; set; }
+		[MaxLength(75)]
+		public string Address_Line_2 { get; set; }
+		[MaxLength(50)]
+		public string City { get; set; }
+		[MaxLength(50)]
+		[JsonProperty(PropertyName = "State/Region")]
+		public string State_Region { get; set; }
+		[MaxLength(15)]
+		public string Postal_Code { get; set; }
+		[MaxLength(50)]
+		public string Foreign_Country { get; set; }
+		[MaxLength(25)]
+		public string Country_Code { get; set; }
+		[MaxLength(10)]
+		public string Carrier_Route { get; set; }
+		[MaxLength(10)]
+		public string Lot_Number { get; set; }
+		[MaxLength(3)]
+		public string Delivery_Point_Code { get; set; }
+		public string Delivery_Point_Check_Digit { get; set; }
+		[MaxLength(15)]
+		public string Latitude { get; set; }
+		[MaxLength(15)]
+		public string Longitude { get; set; }
+		[MaxLength(15)]
+		public string Altitude { get; set; }
+		[MaxLength(50)]
+		public string Time_Zone { get; set; }
+		[MaxLength(50)]
+		public string Bar_Code { get; set; }
+		[MaxLength(50)]
+		public string Area_Code { get; set; }
+		public DateTime? Last_Validation_Attempt { get; set; }
+		[MaxLength(50)]
+		public string County { get; set; }
+		public bool? Validated { get; set; }
+		[Required]
+		public bool Do_Not_Validate { get; set; }
+		public DateTime? Last_GeoCode_Attempt { get; set; }
+		[MaxLength(100)]
+		public string Country { get; set; }
+
+		public const string TableName = "Addresses";
+	}
+}
